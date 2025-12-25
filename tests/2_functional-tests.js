62628d50e89843f298502ed954492577a3c07927
Golam-Rabby821 <golamrabby821@gmail.com>
2025-12-25 10:56:50 +0600

    fixed packages for vulnerability

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,7 +3,9 @@ const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server.js');
 
-chai.use(chaiHttp);
+// chai-http is ESM-only in newer releases; fall back to .default when required via CommonJS
+const chaiHttpPlugin = chaiHttp.default || chaiHttp;
+chai.use(chaiHttpPlugin);
 
 let Translator = require('../components/translator.js');
 
